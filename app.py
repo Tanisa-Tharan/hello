d069ffef196ee7033d53ea8f7f76e9c5c8ed397e
Tanisa-Tharan <60692265+Tanisa-Tharan@users.noreply.github.com>
2020-07-27 20:30:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     string = request.args.get("bla")+""
-    f = open('length.txt','w')
-    f.write(string)
+    #f = open('length.txt','w')
+    #f.write(string)
     """#URL = 'https://www.urlvoid.com/scan/'+string
     #page = requests.get(URL)
     #soup = BeautifulSoup(page.content, 'lxml')
